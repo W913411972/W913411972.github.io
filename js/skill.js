687f85f4783901da6fb04756d21d020b15e6f46c
W913411972 <913411972@qq.com>
2018-12-03 22:57:51 +0800

    换壁纸,微调

--- js/skill.js
+++ js/skill.js
@@ -1,9 +1,14 @@
       function IMGitem(a,b){
+
           $(".IMG").eq(a).hover(function(){
              $(this).next(".progress-bar").stop().animate({
                width: b+"px",
                opacity:"0.9",},
                1000,);
+             $('canvas').stop().animate({
+              opacity: '0.9',
+              },
+              1000,); 
               for (var i = 0; i <= (b/3); i++) {
                  var a = new settime(i);
               }
@@ -12,18 +17,17 @@
                width: "50px",
                opacity:"0",},
                1000,);
+              // clearTimeout(a);
+            $('canvas').stop().animate({
+              opacity: '0',
+              },
+              1000,);  
           });
      }
       function cent(cent){             
                 var width = 3*cent;  
                 return width         
       }     
-       IMGitem(0,cent(80));
-       IMGitem(1,cent(85));
-       IMGitem(2,cent(87));
-       IMGitem(3,cent(88));
-       IMGitem(4,cent(77));
-       IMGitem(5,cent(80));
        var canvas = document.querySelector("canvas");
        var cxt = canvas.getContext("2d");
        function abc(a){
@@ -67,4 +71,9 @@
     function settime(a){
       var t = setTimeout(function(){abc(a)}, a*10)      
     }
-     
+       IMGitem(0,cent(80));
+       IMGitem(1,cent(85));
+       IMGitem(2,cent(87));
+       IMGitem(3,cent(88));
+       IMGitem(4,cent(77));
+       IMGitem(5,cent(80));
